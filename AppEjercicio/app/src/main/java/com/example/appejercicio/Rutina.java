93279bab2635062f94a5799d521c1aec5803e1f8
Jesus Ramirez <jesus.zuluaga@correounivalle.edu.co>
2020-09-24 20:24:01 -0500

    se creó la clase rutina

---
+++ AppEjercicio/app/src/main/java/com/example/appejercicio/Rutina.java
@@ -0,0 +1,53 @@
+package com.example.appejercicio;
+
+import com.example.appejercicio.Ejercicio;
+
+import java.util.ArrayList;
+
+public class Rutina {
+    String id;
+    int repeticiones;
+    int tiempo;
+    ArrayList listaEjercicios;
+
+    //Constructor
+    public void Rutina(){
+
+    }
+
+    //Definiendo los sets
+    public void setId(String id){
+        this.id=id;
+    }
+
+    public void setRepeticiones(int repeticiones){
+        this.repeticiones=repeticiones;
+    }
+
+    public void setTiempo(int tiempo){
+        this.tiempo=tiempo;
+    }
+
+    public void setListaEjercicios(ArrayList listaEjercicios){
+        this.listaEjercicios=listaEjercicios;
+    }
+
+    //Definiendo los gets
+
+    public String getId(){
+        return this.id;
+    }
+
+    public int getRepeticiones(){
+        return this.repeticiones;
+    }
+
+    public int getTiempo(){
+        return this.tiempo;
+    }
+
+    public ArrayList getListaEjercicios(){
+        return this.listaEjercicios;
+    }
+
+}
