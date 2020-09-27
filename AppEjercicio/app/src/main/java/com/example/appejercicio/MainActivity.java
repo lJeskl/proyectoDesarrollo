d49af6580533162fc06befc0ce7d8a23480a0d90
lina aldana <linaaldanavelez@gmail.com>
2020-09-26 21:02:49 -0500

    Se crea vista principal

--- AppEjercicio/app/src/main/java/com/example/appejercicio/MainActivity.java
+++ AppEjercicio/app/src/main/java/com/example/appejercicio/MainActivity.java
@@ -2,17 +2,40 @@ package com.example.appejercicio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 import com.example.appejercicio.Utilidades.Utilidades;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnClickListener {
+
+    private Button btnRegistro;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnRegistro = (Button) findViewById(R.id.btnRegistro);
+        btnRegistro.setOnClickListener(this);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnRegistro){
+            Intent intent = new Intent(this,RegistroActivity.class);
+            startActivity(intent);
+        }else{
+
+        }
 
     }
 }
